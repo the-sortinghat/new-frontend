3285bd318bf78508c3fc9b9f5ff80b199d7483c4
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-05-08 21:40:14 -0700

    change force-graph to vis.js
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- components/Graph/generator.ts
+++ components/Graph/generator.ts
@@ -1,101 +1,29 @@
-import ForceGraph from "force-graph";
-
-function curveLinks(gData: any): any {
-  let selfLoopLinks = {} as any;
-  let sameNodesLinks = {} as any;
-  const curvatureMinMax = 0.5;
-
-  // 1. assign each link a nodePairId that combines their source and target independent of the links direction
-  // 2. group links together that share the same two nodes or are self-loops
-  gData.links.forEach((link: any) => {
-    link.nodePairId =
-      link.source <= link.target
-        ? link.source + "_" + link.target
-        : link.target + "_" + link.source;
-    let map = link.source === link.target ? selfLoopLinks : sameNodesLinks;
-    if (!map[link.nodePairId]) {
-      map[link.nodePairId] = [];
-    }
-    map[link.nodePairId].push(link);
-  });
-
-  // Compute the curvature for self-loop links to avoid overlaps
-  Object.keys(selfLoopLinks).forEach((id) => {
-    let links = selfLoopLinks[id];
-    let lastIndex = links.length - 1;
-    links[lastIndex].curvature = 1;
-    let delta = (1 - curvatureMinMax) / lastIndex;
-    for (let i = 0; i < lastIndex; i++) {
-      links[i].curvature = curvatureMinMax + i * delta;
-    }
-  });
-
-  // Compute the curvature for links sharing the same two nodes to avoid overlaps
-  Object.keys(sameNodesLinks)
-    .filter((nodePairId) => sameNodesLinks[nodePairId].length > 1)
-    .forEach((nodePairId) => {
-      let links = sameNodesLinks[nodePairId];
-      let lastIndex = links.length - 1;
-      let lastLink = links[lastIndex];
-      lastLink.curvature = curvatureMinMax;
-      let delta = (2 * curvatureMinMax) / lastIndex;
-      for (let i = 0; i < lastIndex; i++) {
-        links[i].curvature = -curvatureMinMax + i * delta;
-        if (lastLink.source !== links[i].source) {
-          links[i].curvature *= -1; // flip it around, otherwise they overlap
-        }
-      }
-    });
-
-  return gData;
-}
-
-function dashAsyncLinks(gData: any): any {
-  return {
-    nodes: [...gData.nodes],
-    links: gData.links.map((link: any) => ({
-      ...link,
-      dashed: link.type === "async",
-    })),
-  };
-}
-
-function processLinks(gData: any): any {
-  gData = curveLinks(gData);
-  gData = dashAsyncLinks(gData);
-  return gData;
-}
+import { Node, Edge, Network, Options } from "vis-network";
 
 export default function GraphGenerator(payload: any) {
-  const { data, width, height, container } = payload;
-  const graph = ForceGraph()(container);
+  const { data, container } = payload;
 
-  const gData = processLinks(data);
+  const nodes: Node[] = data.nodes;
 
-  graph
-    .width(width)
-    .height(height)
-    .graphData(gData)
-    .nodeCanvasObject((node: any, canvasCtx) => {
-      canvasCtx.beginPath();
+  const edges: Edge[] = data.edges.map((edge: any) => ({
+    ...edge,
+    arrows: "to",
+    dashes: edge.type === "async",
+    color: { color: "black" },
+  }));
 
-      canvasCtx.fillStyle = "#6b46c1";
-      //canvasCtx.fillRect(node.x - 6, node.y - 4, 12, 8);
-      canvasCtx.arc(node.x, node.y, 3, 0, 2 * Math.PI, false);
-      canvasCtx.stroke();
-      canvasCtx.lineWidth = 0.5;
-      canvasCtx.fill();
+  const options: Options = {
+    nodes: {
+      shape: "dot",
+      size: 12,
+      color: "#6b46c1",
+      font: {
+        size: 12,
+        color: "#000000",
+      },
+      borderWidth: 2,
+    },
+  };
 
-      canvasCtx.fillStyle = "black";
-      canvasCtx.font = `3px 'Montserrat'`;
-      canvasCtx.textAlign = "center";
-      canvasCtx.textBaseline = "middle";
-      canvasCtx.fillText(node.name, node.x, node.y + 6);
-    })
-    .linkCurvature("curvature")
-    .linkLineDash((link: any) => link.dashed && [2, 2])
-    .linkWidth(2)
-    .linkDirectionalArrowLength(6)
-    .linkDirectionalArrowRelPos(1)
-    .zoom(5);
+  new Network(container, { nodes, edges }, options);
 }
--- components/Graph/index.tsx
+++ components/Graph/index.tsx
@@ -5,34 +5,27 @@ import styles from "./styles.module.css";
 const Graph: React.FC = () => {
   const data = {
     nodes: [
-      { id: 1, name: "Actuator Controller" },
-      { id: 2, name: "Data Collector" },
-      { id: 3, name: "Resource Adaptor" },
-      { id: 4, name: "Resource Catalog" },
-      { id: 5, name: "Resource Discovery" },
+      { id: 1, label: "Actuator Controller" },
+      { id: 2, label: "Data Collector" },
+      { id: 3, label: "Resource Adaptor" },
+      { id: 4, label: "Resource Catalog" },
+      { id: 5, label: "Resource Discovery" },
     ],
-    links: [
-      { source: 1, target: 2, type: "sync" },
-      { source: 1, target: 3, type: "sync" },
-      { source: 3, target: 2, type: "async" },
-      { source: 3, target: 4, type: "async" },
-      { source: 4, target: 5, type: "async" },
-      { source: 5, target: 4, type: "sync" },
-      { source: 5, target: 3, type: "async" },
-      { source: 5, target: 3, type: "sync" },
-      { source: 5, target: 2, type: "async" },
+    edges: [
+      { from: 1, to: 2, type: "sync" },
+      { from: 1, to: 3, type: "sync" },
+      { from: 3, to: 2, type: "async" },
+      { from: 3, to: 4, type: "async" },
+      { from: 4, to: 5, type: "async" },
+      { from: 5, to: 4, type: "sync" },
+      { from: 5, to: 3, type: "async" },
+      { from: 5, to: 3, type: "sync" },
+      { from: 5, to: 2, type: "async" },
     ],
   };
 
   useEffect(() => {
-    const frame = document.getElementById("graph");
-
-    GraphGenerator({
-      data,
-      width: frame?.clientWidth,
-      height: frame?.clientHeight,
-      container: frame,
-    });
+    GraphGenerator({ data, container: document.getElementById("graph") });
   });
 
   return <div id="graph" className={styles.graph}></div>;
