36f75db3a5a007928c34376281b1870a267b0905
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-12-13 19:56:01 -0300

    redirect when redirect url is provided as env var
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    const url = process.env.NEXT_PUBLIC_REDIRECT_URL;
+
+    if (url) {
+      return ["/", "/systems/:id", "/systems/register"].map((source) => ({
+        source,
+        destination: url,
+        permanent: true,
+      }));
+    }
+
+    return [];
+  },
+};
+
+module.exports = nextConfig;
